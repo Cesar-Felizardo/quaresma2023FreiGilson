83e0bc65e80d24079eee1d4fce464e1fa53df79f
Pregacoes <pregacoes.catolica@gmail.com>
2023-03-23 13:42:25 -0300

    First commit

---
+++ quresma.js
@@ -0,0 +1,8 @@
+
+setInterval( () => {
+    window.addEventListener('load', function() {
+        let btnMenu = document.getElementById("#btnMenu")
+        btnMenu.classList.toggle("toggleColor")
+    })
+    
+},1000)
