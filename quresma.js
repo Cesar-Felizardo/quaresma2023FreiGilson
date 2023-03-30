42a7bb61e951af1dec26c23564e128440b0d1901
CESAR FELIZARDO <70537620+Cesar-Felizardo@users.noreply.github.com>
2023-03-30 16:21:25 -0300

    Tela de carregamento...

--- quresma.js
+++ quresma.js
@@ -1,17 +1,8 @@
-/* window.addEventListener('load', function() {
-    let body = document.querySelector("body")
-    body.classList.add("waitLoad")
 
-    init = 5
-    setInterval(function() {
-        if(init <= 0) {
-            body.classList.remove("waitLoad")
-        }
-        else {
-            init--
-        }
-    },1000)
-}) */
+  window.addEventListener("load", function() {
+    const loader = document.querySelector("#loader");
+    loader.classList.add("hide");
+  });
 
 
 
